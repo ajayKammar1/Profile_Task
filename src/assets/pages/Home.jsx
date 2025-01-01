f713746b29dfd733270667e08436cac5003f1f2d
Ajay Kammar <kammarajay1@gmail.com>
2025-01-01 16:08:48 +0530

    React Basic Folder Structure and File

---
+++ src/assets/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Hedder from "../components/Hedder";
+
+const Home = () => {
+  return (
+    <div>
+      <Hedder />
+    </div>
+  );
+};
+
+export default Home;
